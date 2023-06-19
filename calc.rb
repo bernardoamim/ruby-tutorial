fef17b4bc739b538cef0dbbb9910b1a68533bdf8
bernardo.amim <bernardo.amim@mercatus.com>
2023-06-19 20:38:33 -0300

    feat: finishing first exercise

--- calc.rb
+++ calc.rb
@@ -5,5 +5,11 @@ print "Enter the second integer number: "
 number2 = gets.chomp.to_i
 
 sum = number1 + number2
+subtraction = number1 + number2
+mult = number1 + number2
+division = number1 + number2
 
 puts "The result of the sum is: #{sum}"
+puts "The result of the subtraction is: #{subtraction}"
+puts "The result of the multiplication is: #{mult}"
+puts "The result of the division is: #{division}"
