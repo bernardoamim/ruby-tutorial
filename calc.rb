86226f2b6ebda51969040f68f69223dbd0ef6ef1
bernardo.amim <bernardo.amim@mercatus.com>
2023-06-19 20:35:43 -0300

    feat: inital commit

---
+++ calc.rb
@@ -0,0 +1,9 @@
+print "Enter the first integer number: "
+number1 = gets.chomp.to_i
+
+print "Enter the second integer number: "
+number2 = gets.chomp.to_i
+
+sum = number1 + number2
+
+puts "The result of the sum is: #{sum}"
---
+++ hello.rb
@@ -0,0 +1,7 @@
+print "Enter your name: "
+name = gets.chomp
+
+print "Enter your last name: "
+last_name = gets.chomp
+
+puts "Hello #{name} #{last_name}!"
